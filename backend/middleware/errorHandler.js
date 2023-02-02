effe3aea2049b8c1dc0fbd8b872096e7de0eedaa
José Lin <lin-jose@outlook.com>
2023-02-02 00:56:38 -0300

    Add errorHandler middleware

---
+++ backend/middleware/errorHandler.js
@@ -0,0 +1,15 @@
+const { logEvents } = require("./logger");
+
+const errorHandler = (error, req, res, next) => {
+  logEvents(
+    `${error.name}: ${error.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
+    "errorLog.log"
+  );
+  console.log(error.log);
+
+  res.status(res.statusCode ? res.statusCode : 500);
+
+  res.json({ message: error.message });
+};
+
+module.exports = errorHandler;
