77bb21174b9908f904a3f0c3f0955142880c9c93
José Lin <lin-jose@outlook.com>
2023-02-02 01:12:31 -0300

    Add necessary 3rd party middleware

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -5,7 +5,7 @@ const errorHandler = (error, req, res, next) => {
     `${error.name}: ${error.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
     "errorLog.log"
   );
-  console.log(error.log);
+  console.log(error.stack);
 
   res.status(res.statusCode ? res.statusCode : 500);
 
--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,16 @@ const app = express();
 const path = require("path");
 const { logger } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const corsOptions = require("./config/corsOptions");
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 
 app.use(express.static("public"));
 
