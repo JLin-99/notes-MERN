7b791413840cf77cdccf0d9da757867a6c118959
José Lin <lin-jose@outlook.com>
2023-02-03 20:16:48 -0300

    Fetch notes from backend

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -14,7 +14,20 @@ const createNote = async (noteData, token) => {
   return response.data;
 };
 
+const getNotes = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL, config);
+
+  return response.data;
+};
+
 const noteService = {
   createNote,
+  getNotes,
 };
 export default noteService;
--- frontend/src/pages/Board.jsx
+++ frontend/src/pages/Board.jsx
@@ -1,10 +1,31 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getNotes, reset } from "../features/notes/noteSlice";
 import NewNote from "../components/NewNote";
+import Spinner from "../components/Spinner";
 
 const Board = () => {
+  const { notes, isLoading, isSuccess } = useSelector((state) => state.note);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      if (isSuccess) {
+        dispatch(reset());
+      }
+    };
+  }, [dispatch, isSuccess]);
+
+  useEffect(() => {
+    dispatch(getNotes());
+  }, [dispatch]);
+
+  if (isLoading) return <Spinner />;
+
   return (
     <div className="grow">
       <h2>Board</h2>
-      <NewNote />
+      {/* <NewNote /> */}
     </div>
   );
 };
